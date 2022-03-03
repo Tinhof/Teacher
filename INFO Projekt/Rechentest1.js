cda961c95d7fae0ae92df579ceb87b8dc8bbca1e
Tinhof <99043095+Tinhof@users.noreply.github.com>
2022-03-03 15:36:45 +0100

    Add files via upload
    Projekt Ordner

---
+++ INFO Projekt/Rechentest1.js
@@ -0,0 +1,11 @@
+//10+7=b;
+
+function Addition2()
+{
+	var b;
+	b = document.getElementById("InputNumber2").value;
+if(b==17)
+    {document.getElementById("Addition2").innerHTML="Richtig"}
+else
+    {document.getElementById("Addition2").innerHTML="Falsch"}
+}
